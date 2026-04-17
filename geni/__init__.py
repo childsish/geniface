9c22062b1c3bb2b2ace8ae8a3317f56c4f1cf6a7
Liam H. Childs <liam.childs@pei.de>
2026-04-17 14:17:47 +0200

    rename geniface to geni

--- geni/__init__.py
+++ geni/__init__.py
@@ -1,3 +1,3 @@
-from geniface.ir import FieldSpec, FunctionSpec, call_function, inspect_function
+from geni.ir import FieldSpec, FunctionSpec, call_function, inspect_function
 
 __all__ = ["FieldSpec", "FunctionSpec", "call_function", "inspect_function"]
